1a06a9e4db51b0c9eceb83a392c5c16a82903832
Lukas Zradula <luke1258@seznam.cz>
2023-10-05 20:28:45 +0200

    added random number + prevod meny

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Random number
 const number = Math.random()
 
-document.body.innerHTML = number
+document.body.innerHTML = number + '<br>'
+
+// Prevod meny
+const wageInEur = 20
+const rate = 24.55
+let wageInCzk = Math.round(wageInEur * rate)
+
+document.body.innerHTML = '<h1>' + wageInCzk + '</h1>' + '<br>'
+
