ab120646d26119a19aa843e0e817aef68f68dcec
Lukas Zradula <luke1258@seznam.cz>
2023-10-05 20:47:18 +0200

    added ultramaraton

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 // Random number
 const number = Math.random()
 
-document.body.innerHTML = number + '<br>'
+document.body.innerHTML += number + '<br>'
 
 // Prevod meny
 const wageInEur = 20
 const rate = 24.55
 let wageInCzk = Math.round(wageInEur * rate)
 
-document.body.innerHTML = '<h1>' + wageInCzk + '</h1>' + '<br>'
+document.body.innerHTML += '<h1>' + wageInCzk + '</h1>' + '<br>'
 
+// Ultramaraton
+start = 15
+length = 10
+end = (start + length) % 24
+
+document.body.innerHTML += end
